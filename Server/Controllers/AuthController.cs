7bf674e4d2c925bf22da4a1286fca8ffb2853b98
Claudio Schaad <c.schaad@pog.ch>
2025-08-27 21:18:21 +0200

    Remove unused unsing

--- Server/Controllers/AuthController.cs
+++ Server/Controllers/AuthController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
